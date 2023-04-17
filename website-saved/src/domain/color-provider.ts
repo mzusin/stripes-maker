4dc2d93fd54b161a570b6997fe1aef17a9d95dc7
Miriam Zusin <miriamzusin@gmail.com>
2023-04-17 20:46:02 +0300

    del

--- website-saved/src/domain/color-provider.ts
+++
@@ -1,5 +0,0 @@
-import { IColor } from '../data/slices/form-slice';
-
-export const rgbaColorToString = (rgbaColor: IColor) => {
-    return `rgba(${ rgbaColor.r }, ${ rgbaColor.g }, ${ rgbaColor.b }, ${ rgbaColor.a })`;
-};
--- website-saved/src/enums/AnimationTypeEnum.ts
+++
@@ -1,6 +0,0 @@
-export enum AnimationTypeEnum {
-    NoAnimation = 1,
-    RotateClockWise = 2,
-    RotationCounterClockwise = 3,
-    Linear = 4,
-}
